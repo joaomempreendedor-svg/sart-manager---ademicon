b33749c2dce07115017cb1b29e8f37aa83fa38aa
[dyad] <joaomempreendedor@gmail.com>
2025-12-15 00:11:49 -0300

    [dyad] Corrigindo erro de parsing do ScrollArea com asserção de tipo final - wrote 1 file(s)

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -6,10 +6,13 @@ import { cn } from "@/lib/utils"
 // Definindo as propriedades do ScrollBar
 interface ScrollBarProps extends React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar> {}
 
-// Chamando React.forwardRef sem argumentos genéricos explícitos
+// Definindo o tipo da ref separadamente para clareza
+type ScrollBarRef = React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>;
+
+// Removendo *todos* os argumentos genéricos de React.forwardRef
 const ScrollBar = React.forwardRef((
-  { className, orientation = "vertical", ...props }: ScrollBarProps, // Props tipadas diretamente aqui
-  ref: React.Ref<React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>> // Ref tipada diretamente aqui
+  { className, orientation = "vertical", ...props }: ScrollBarProps, // Props tipadas diretamente na função
+  ref: React.Ref<ScrollBarRef> // Ref tipada diretamente na função
 ) => (
   <ScrollAreaPrimitive.ScrollAreaScrollbar
     ref={ref}
@@ -26,7 +29,7 @@ const ScrollBar = React.forwardRef((
   >
     <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-gray-300 dark:bg-slate-600" />
   </ScrollAreaPrimitive.Scrollbar>
-)) as React.ForwardRefExoticComponent<ScrollBarProps & React.RefAttributes<React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>>>; // Asserção de tipo no resultado
+)) as React.ForwardRefExoticComponent<ScrollBarProps & React.RefAttributes<ScrollBarRef>>; // Asserção de tipo no resultado final
 
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
 
