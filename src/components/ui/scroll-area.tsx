f8888b802cd75237bec17378473b79244b86ce93
[dyad] <joaomempreendedor@gmail.com>
2025-12-15 00:47:35 -0300

    [dyad] Implementando componentes shadcn/ui ausentes - wrote 6 file(s)

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -1,18 +1,32 @@
+"use client"
+
 import * as React from "react"
 import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area"
 
 import { cn } from "@/lib/utils"
 
-// Define props interfaces
-interface ScrollBarProps extends React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar> {}
-interface ScrollAreaRootProps extends React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root> {}
+const ScrollArea = React.forwardRef<
+  React.ElementRef<typeof ScrollAreaPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
+>(({ className, children, ...props }, ref) => (
+  <ScrollAreaPrimitive.Root
+    ref={ref}
+    className={cn("relative overflow-hidden", className)}
+    {...props}
+  >
+    <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
+      {children}
+    </ScrollAreaPrimitive.Viewport>
+    <ScrollBar />
+    <ScrollAreaPrimitive.Corner />
+  </ScrollAreaPrimitive.Root>
+))
+ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName
 
-// ScrollBar component
-// Define a função do componente separadamente, com tipagem nos parâmetros
-const ScrollBarComponent = (
-  { className, orientation = "vertical", ...props }: ScrollBarProps,
-  ref: React.Ref<React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>>
-) => (
+const ScrollBar = React.forwardRef<
+  React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
+>(({ className, orientation = "vertical", ...props }, ref) => (
   <ScrollAreaPrimitive.ScrollAreaScrollbar
     ref={ref}
     orientation={orientation}
@@ -26,37 +40,9 @@ const ScrollBarComponent = (
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-gray-300 dark:bg-slate-600" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
-);
-
-// Envolve a função com React.forwardRef e aplica a asserção de tipo no resultado
-const ScrollBar = React.forwardRef(ScrollBarComponent) as React.ForwardRefExoticComponent<ScrollBarProps & React.RefAttributes<React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>>>;
-
+))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
-
-// ScrollArea component
-// Define a função do componente separadamente, com tipagem nos parâmetros
-const ScrollAreaComponent = (
-  { className, children, ...props }: ScrollAreaRootProps,
-  ref: React.Ref<React.ElementRef<typeof ScrollAreaPrimitive.Root>>
-) => (
-  <ScrollAreaPrimitive.Root
-    ref={ref}
-    className={cn("relative overflow-hidden", className)}
-    {...props}
-  >
-    <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
-      {children}
-    </ScrollAreaPrimitive.Viewport>
-    <ScrollBar />
-    <ScrollAreaPrimitive.Corner />
-  </ScrollAreaPrimitive.Root>
-);
-
-// Envolve a função com React.forwardRef e aplica a asserção de tipo no resultado
-const ScrollArea = React.forwardRef(ScrollAreaComponent) as React.ForwardRefExoticComponent<ScrollAreaRootProps & React.RefAttributes<React.ElementRef<typeof ScrollAreaPrimitive.Root>>>;
-
-ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName
 
 export { ScrollArea, ScrollBar }
