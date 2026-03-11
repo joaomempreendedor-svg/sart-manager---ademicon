fbe9f3056903707e822d22df50a2bd471188e608
[dyad] <joaomempreendedor@gmail.com>
2026-03-11 15:43:35 -0300

    [dyad] Moving Sonner Toaster inside AppProvider to resolve context error - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -148,6 +148,7 @@ const App = () => {
     <HashRouter>
       <AuthProvider>
         <AppProvider>
+          <Toaster /> {/* Adicionar Sonner Toaster aqui */}
           <AppRoutes />
         </AppProvider>
       </AuthProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Toaster } from './components/ui/sonner'; // Importar Toaster como named export
 import './index.css';
 
 const rootElement = document.getElementById('root');
@@ -12,7 +11,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <Toaster /> {/* Adicionar Sonner Toaster aqui */}
+    {/* Toaster moved to App.tsx to be within AppProvider */}
     <App />
   </React.StrictMode>
 );
